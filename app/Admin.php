b72df3332fc6741082a12fdf26dac124be69c300
khagag <the_lord29@yahoo.com>
2020-09-13 13:22:00 +0200

    [modify] namespace of admin and student

--- app/Admin.php
+++ app/Admin.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use  Notifiable;
 
 
     protected $guard = 'admin';
--- app/Student.php
+++ app/Student.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class Student extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     protected $guard = 'student';
     /**
