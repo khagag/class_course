a511bd1b64bf4710bfd1168aa6296a48893d09b1
khagag <the_lord29@yahoo.com>
2020-09-14 05:04:41 +0200

    [create] student middleware

---
+++ app/Http/Middleware/student.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class student
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+       if (Auth::check()){
+         if ($guard == "student" && Auth::guard($guard)->check()) {
+           return $next($request);
+         }
+       }
+
+        return redirect('/');
+    }
+}
