34c3a38c992de264154ac00bfceefc5aaecc609a
khagag <the_lord29@yahoo.com>
2020-09-14 02:58:42 +0200

    [add] define admin sidebar course & lesson links

--- resources/views/layouts/adminDashBoard.blade.php
+++ resources/views/layouts/adminDashBoard.blade.php
@@ -7,8 +7,8 @@
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item"><a href="{{route('student.index')}}" class="card-link">students</a></li>
-      <li class="list-group-item"><a href="#" class="card-link">courses</a></li>
-      <li class="list-group-item"><a href="#" class="card-link">lessons</a></li>
+      <li class="list-group-item"><a href="{{route('course.index')}}" class="card-link">courses</a></li>
+      <li class="list-group-item"><a href="{{route('lesson.index')}}" class="card-link">lessons</a></li>
     </ul>
   </div>
 @endsection
